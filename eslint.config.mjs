49a6b9bd6a90a3a6ff72cecbe5b88618eca732af
pixelrabbit <dangovea@gmail.com>
2026-02-12 17:24:11 -0600

    add deploy file

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,10 @@
 import js from "@eslint/js";
-import prettier from "eslint-plugin-prettier/recommended";
 import tseslint from "typescript-eslint";
 
 export default tseslint.config(
   { ignores: ["dist", "src/engine/**/*.ts"] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended, prettier],
+    extends: [js.configs.recommended, ...tseslint.configs.recommended],
     files: ["**/*.{ts,tsx}"],
     languageOptions: {
       ecmaVersion: "latest",
